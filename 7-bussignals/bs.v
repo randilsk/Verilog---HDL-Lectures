c76fcf79ba6939be728fa5c0641f0019d46fc1a8
randilsk <randilsulakkana@gmail.com>
2025-04-08 15:37:47 +0530

    lec2ii

--- 7-bussignals/bs.v
+++ 7-bussignals/bs.v
@@ -1,3 +1,19 @@
+// module bsignal(
+//     input wire [3:0] a,
+//     input wire [3:0] b,
+//     input wire c,
+//     output wire [3:0] p
+// );
+
+// assign p[0]=(a[0]&(~c))|(b[0]&c);
+// assign p[1]=(a[1]&(~c))|(b[1]&c);
+// assign p[2]=(a[2]&(~c))|(b[2]&c);   
+// assign p[3]=(a[3]&(~c))|(b[3]&c);
+
+// endmodule
+
+
+
 module bsignal(
     input wire [3:0] a,
     input wire [3:0] b,
@@ -5,12 +21,9 @@ module bsignal(
     output wire [3:0] p
 );
 
-assign p[0]=(a&(~c))|(b&c);
-assign p[1]=(a&(~c))|(b&c); 
-assign p[2]=(a&(~c))|(b&c);
-assign p[3]=(a&(~c))|(b&c);
+wire [3:0] cbus;
+
+assign cbus={4{c}};
+assign p=(a&(~cbus))|(b&cbus);
 
 endmodule
-
-
-
